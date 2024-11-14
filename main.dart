f85321f6bcea9a744e1b79379e10810e1611a55a
rafkaputra221 <rafkaputra2007@gmail.com>
2024-11-14 10:34:03 +0700

    Tutorial Layout

--- main.dart
+++ main.dart
@@ -1,69 +1,190 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
+    const String appTitle = 'Flutter layout demo';
     return MaterialApp(
+      title: appTitle,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Container Text Example'),
-        ),
-        body: Center(
-          child: Container(
-            width: double.infinity,
-            margin: const EdgeInsets.all(12),
-            height: 200,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              gradient: const LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 103, 147, 90),
-                  Color.fromARGB(255, 142, 194, 83),
-                  Color.fromARGB(255, 72, 216, 82),
-                  Color.fromARGB(255, 47, 113, 38),
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
+          appBar: AppBar(
+            title: const Text(appTitle),
+          ),
+          body: const SingleChildScrollView(
+            child: Column(children: [
+              ImageSection(
+                image: 'assets/labuanbajo.jpg',
               ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0), // Adjust padding as needed
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start, // Left-align text
-                children: [
-                  const Text(
-                    'membuat container widget',
-                    style: TextStyle(
-                      fontSize: 25,
+              TitleSection(
+                name: 'Labuan Bajo',
+                location: 'Komodo District, West Manggarai Regency, East Nusa Tenggara Province',
+              ),
+              ButtonSection(),
+              TextSection(
+                description:
+                    'Labuan Bajo, a name that is increasingly popular in the ears of travelers, '
+                    'is a tourist destination that offers extraordinary natural beauty.'
+                    'Located at the western tip of Flores Island, East Nusa Tenggara, '
+                    'Labuan Bajo is the main gateway to Komodo National Park, which is famous for the presence of Komodo dragons, the largest lizard in the world.'
+                    ''
+              ),
+            ]),
+          )),
+    );
+  }
+}
+
+class TitleSection extends StatelessWidget {
+  const TitleSection({
+    super.key,
+    required this.name,
+    required this.location,
+  });
+
+  final String name;
+  final String location;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    name,
+                    style: const TextStyle(
                       fontWeight: FontWeight.bold,
-                      color: Colors.white,
                     ),
                   ),
-                  const SizedBox(height: 8),
-                  const Divider(color: Colors.black),
-                  const Text(
-                    'Dreams cannot come true by themselves, but dreams will come when someone tries to achieve them.',
-                    style: TextStyle(color: Colors.white),
+                ),
+                Text(
+                  location,
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 153, 153, 153),
                   ),
-                  const SizedBox(height: 20),
-                  SizedBox(
-                    height: 40,
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text(' Flutter '),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: const Color.fromARGB(255, 255, 221, 0),
+          ),
+          const Text('400M'),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Color color = Theme.of(context).primaryColor;
+    return SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ButtonWithText(
+            color : Colors.black,
+            icon: Icons.call,
+            label: 'CALL',
+          ),
+          ButtonWithText(
+        color : Colors.black,
+            icon: Icons.near_me,
+            label: 'ROUTE',
+          ),
+          ButtonWithText(
+            color : Colors.black,
+            icon: Icons.share,
+            label: 'SHARE',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonWithText extends StatelessWidget {
+  const ButtonWithText({
+    super.key,
+    required this.color,
+    required this.icon,
+    required this.label,
+  });
+
+  final Color color;
+  final IconData icon;
+  final String label;
+
+ @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
             ),
           ),
         ),
+      ],
+    );
+  }
+}
+
+class TextSection extends StatelessWidget {
+  const TextSection({
+    super.key,
+    required this.description,
+  });
+
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        description,
+        softWrap: true,
       ),
+    );
+  }
+}
+
+class ImageSection extends StatelessWidget {
+  const ImageSection({super.key, required this.image});
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      image,
+      width: 600,
+      height: 240,
+      fit: BoxFit.cover,
     );
   }
 }
